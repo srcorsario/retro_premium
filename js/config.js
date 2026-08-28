f2383436e673c2274196e3268c12ac8cff805183
Sr Corsario <116904782+srcorsario@users.noreply.github.com>
2026-08-29 00:29:20 +0200

    Update config.js

--- js/config.js
+++ js/config.js
@@ -4,7 +4,7 @@ const ENV = {
     SHEET_ID: '2PACX-1vQitSKxbhg-LRaczPO4vsSxS4rA9jV2N3uz7xnyLu7-ufTzWW-2Zm9arK_zL_18C84kma3xvlIua32_',
     
     // URL de tu Apps Script (Para el futuro módulo de escritura)
-    API_URL: 'https://script.google.com/macros/s/AKfycbxVSoyUb3cE-9bZYrSmaxqhtEcqAdEQkj5fypquxMvzQ5grUkVaqBCq6lDPHSWA7xS1/exec', 
+    API_URL: 'https://script.google.com/macros/s/AKfycbyMNdLxlidsCL8KvOwZlupUgfLw7kWDh9ppKWXcqeCu4pSSg66ApBcpFYaYVPjUzgtT/exec', 
 
     // MAPEO EXACTO DE PESTAÑAS (Nombre en el HTML vs GID de Google)
     SHEETS: {
