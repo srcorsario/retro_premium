1d2e268ba465eb22a4f9b2aa9f671af438ca57bb
Sr Corsario <116904782+srcorsario@users.noreply.github.com>
2026-09-20 16:53:40 +0200

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -3,8 +3,8 @@ const ENV = {
     // ID público de tu hoja
     SHEET_ID: '2PACX-1vQitSKxbhg-LRaczPO4vsSxS4rA9jV2N3uz7xnyLu7-ufTzWW-2Zm9arK_zL_18C84kma3xvlIua32_',
     
-    // URL de tu Apps Script (nueva implementación 2026-09-20, añade "Montar Kit")
-    API_URL: 'https://script.google.com/macros/s/AKfycbxTrDOtTRsTQLN8CURem7iG5mbJmnuyjyHMvL0-DRjcWUglxBOeXivMWZAvBrgNgr1P/exec',
+    // URL de tu Apps Script (nueva implementación 2026-09-20, "Quién tiene acceso: Cualquier usuario")
+    API_URL: 'https://script.google.com/macros/s/AKfycby-wbVT25qvHONcWCgmAhQB3hAfulwVL8lkBSkk_UNf3SiW_Ap0gwIY1GICXFDcQMGI/exec',
 
     // MAPEO EXACTO DE PESTAÑAS (Nombre en el HTML vs GID de Google)
     SHEETS: {
